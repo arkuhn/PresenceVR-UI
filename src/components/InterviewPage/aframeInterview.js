ae1bde2f2d49ba70c4306a60c0826eb9bc00d7e4
Adam Kuhn <ark9719@rit.edu>
2018-12-10 16:19:57 -0500

    Install and update aframe environment

--- src/components/InterviewPage/aframeInterview.js
+++ src/components/InterviewPage/aframeInterview.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './aframeInterview.css';
 import 'aframe';
 import {Entity, Scene} from 'aframe-react';
+import 'aframe-environment-component'
 
 class AframeInterview extends Component {
 
@@ -9,7 +10,7 @@ class AframeInterview extends Component {
         return (
             <Scene className="aframeContainer" embedded> 
                 <Entity id="box" geometry="primitive: box" material="color: red"></Entity>
-               
+                <Entity environment={{preset: 'default', dressingAmount: 500}}></Entity>
             </Scene>
         )
     }
