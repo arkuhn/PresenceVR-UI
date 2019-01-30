b9017b904d91d236b92f57b7e39877cb7e851e75
Adam Kuhn <ark9719@rit.edu>
2019-01-30 13:53:07 -0500

    Reload interview callback

--- src/components/InterviewPage/assets.js
+++ src/components/InterviewPage/assets.js
@@ -8,25 +8,30 @@ import UploadAPI from '../../utils/UploadAPI';
 
 registerPlugin(FilePondPluginImagePreview);
 
-function renderAsset() {
-        //TODO PATCH update interview 'rendered assets' property and reload page
-}
+class Asset extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-function Asset(props) {
-    return (
-    
-    <List.Item active={false} >
-    <List.Content floated='left'>
-            <Icon name={props.icon} />
-            <b>{props.name}</b> <br/>
-            {props.owner}
-        </List.Content>
+    renderAsset = (event) =>  {
+        this.props.updateInterviewCallback()
+    }
 
-        <List.Content floated='right'>
-            <Checkbox toggle onClick={renderAsset}/>
-        </List.Content>
-    </List.Item>
-    )
+    render() {
+        return (
+            <List.Item active={false} >
+            <List.Content floated='left'>
+                    <Icon name={this.props.icon} />
+                    <b>{this.props.name}</b> <br/>
+                    {this.props.owner}
+                </List.Content>
+        
+                <List.Content floated='right'>
+                    <Checkbox toggle onClick={this.renderAsset}/>
+                </List.Content>
+            </List.Item>
+            )
+    }
 }
 
 
@@ -39,23 +44,17 @@ class Assets extends Component {
             uploadedFile: '',
             loading: false
         }
-
-        this.handleModelClose = this.handleModelClose.bind(this)
-        this.handleModelOpen = this.handleModelOpen.bind(this)
-        this.loadAssetsModal = this.loadAssetsModal.bind(this);
-        this.generateAssets = this.generateAssets.bind(this)
-        this.onChange = this.onChange.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
     }
-    handleModelClose() { 
+
+    handleModelClose = () => { 
         this.setState({ modalOpen: false })
     }
 
-    handleModelOpen() {
+    handleModelOpen = () => {
         this.setState({ modalOpen: true})
     }
 
-    updateList() {
+    updateList = () => {
         this.setState({loading: true})
         UploadAPI.getUploads().then((uploads) => {  
             if (uploads && uploads.data) {
@@ -67,18 +66,11 @@ class Assets extends Component {
         });
     }
 
-    componentDidMount() {
+    componentDidMount = () => {
         this.updateList()
     }
 
-    renderAssets() {
-                //TODO update interview 'rendered assets' property and reload page
-
-    }
-
     onChange = (e) => {
-        console.log(e.target)
-
         switch (e.target.name) {
           case 'uploadedFile':
             this.setState({ uploadedFile: e.target.files[0] });
@@ -86,10 +78,9 @@ class Assets extends Component {
           default:
             this.setState({ [e.target.name]: e.target.value });
         }
-        console.log(this.state)
       }
 
-    onSubmit(e) {
+    onSubmit = (e) => {
         e.preventDefault();
         let formData = new FormData();
         if (!this.state.uploadedFile) {
@@ -105,7 +96,7 @@ class Assets extends Component {
     }
 
 
-    loadAssetsModal() {
+    loadAssetsModal = () => {
         return (
             <form onSubmit={this.onSubmit}>
 
@@ -121,7 +112,7 @@ class Assets extends Component {
             </form>
         )    
     }
-    generateAssets() {
+    generateAssets = () => {
         if (this.state.loading) {
             return (<div>
                 <br/>
@@ -142,7 +133,7 @@ class Assets extends Component {
     
         return this.state.assets.map((asset) => {
             return  (  
-                <Asset name={asset.name} owner={asset.owner} icon='boxes'/>
+                <Asset name={asset.name} owner={asset.owner} icon='boxes' updateInterviewCallback={this.props.updateInterviewCallback}/>
             )
             
         })
