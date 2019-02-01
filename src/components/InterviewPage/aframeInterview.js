e6cf9b5ed71566ff6cec13672b160ca91e38116c
jxd3700 <jxd3700@g.rit.edu>
2019-02-01 13:29:21 -0500

    positioning logic for assets added

--- src/components/InterviewPage/aframeInterview.js
+++ src/components/InterviewPage/aframeInterview.js
@@ -20,15 +20,15 @@ class AframeInterview extends Component {
     //Not sure why env prop updates but not loadedAssets, this is a hack to fix
     componentWillReceiveProps(data) {
         if (data.loadedAssets) {
+            var index = 0
             var assetPromises = data.loadedAssets.map((assetId) => {
                 return UploadAPI.getUpload(assetId).then((asset) => {
                     if (asset) {
                         console.log(asset)
-                        var randomX = Math.random() * (6 - 1) + 1
-                        var randomY = Math.random() * (6 - 1) + 1
-                        var randomZ = Math.random() * (2 - 0) + 0
-                        var randomD = Math.random() * (6 - 2) + 1
-                        return <Entity geometry={{primitive: 'box', width: 0.001, height: randomD}} material={{src: `${API_URL}${asset.data.fullpath}`}} position={{x: randomX, y: randomY, z: randomZ}} /> 
+                        var posX = 0
+                        var posY = 2.5
+                        index = index - 3
+                        return <Entity geometry={{primitive: 'box', depth: 0.001}} material={{src: `${API_URL}${asset.data.fullpath}`}} position={{x: posX, y: posY, z: index}} /> 
                     
                     }
                 })
@@ -46,7 +46,7 @@ class AframeInterview extends Component {
             <Scene className="aframeContainer" embedded> 
                 <Entity id="box" geometry="primitive: box" material="color: red"></Entity>
                 <Entity environment={{preset: this.props.environment, dressingAmount: 500}}></Entity>
-                <a-light type="point" color="white" position="0 5 0"></a-light>
+                <a-light type="point" color="white" position="3 5 5"></a-light>
 
                 <Entity geometry={{primitive: 'box'}} material={{src: `https://b3h2.scene7.com/is/image/BedBathandBeyond/185908365252857p?$imagePLP$&wid=256&hei=256`}} position={{x: 0, y: 5, z: -10}} />
                 {this.state.renderedAssets}
