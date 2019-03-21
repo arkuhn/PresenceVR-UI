b7cbd80f202acbd3973d63519510ae32b45f9bd3
William Estey <wpe2632@mail.rit.edu>
2019-03-21 18:02:55 -0400

    Converted to npm installed versions of aframe and networked-aframe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-//import Homepage from "./components/Homepage/Homepage";
-//import InterviewPage from "./components/InterviewPage/InterviewPage";
-//import LoginPage from "./components/LoginPage/LoginPage";
-//import UploadPage from "./components/UploadPage/Upload";
+import Homepage from "./components/Homepage/Homepage";
+import InterviewPage from "./components/InterviewPage/InterviewPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import UploadPage from "./components/UploadPage/Upload";
 import aframe_string from "./js/aframe.js";
 var aframe_html = { __html: aframe_string };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
-//import './index.css';
-//import * as serviceWorker from './serviceWorker';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
