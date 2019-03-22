3755b6d711eb67464cc49a7b2a372cf55613af03
Austin Wolf <austinwolf@Austins-MacBook-Pro.local>
2019-03-22 13:16:29 -0400

    init commit on video chat

--- src/components/InterviewPage/InterviewPage.js
+++ src/components/InterviewPage/InterviewPage.js
@@ -10,11 +10,12 @@ import ChatPane from "./chat";
 import Environments from "./environments";
 import './InterviewPage.css';
 import Participants from "./participants";
-import Host from "./host"
-import InterviewForm from "../InterviewCard/InterviewForm"
-import CancelInterview from "../InterviewCard/cancelInterview"
-import LeaveInterview from "../InterviewCard/leaveInterview"
-import Configuration from "./configuration"
+import Host from "./host";
+import InterviewForm from "../InterviewCard/InterviewForm";
+import CancelInterview from "../InterviewCard/cancelInterview";
+import LeaveInterview from "../InterviewCard/leaveInterview";
+import Configuration from "./configuration";
+import VideoComponent from "./videoComponent";
 
 class InterviewPage extends Component {
     constructor(props) {
@@ -157,19 +158,7 @@ class InterviewPage extends Component {
                     <Grid.Column width={8}>
                         {/* Browser mode */}
                         <Grid.Row>
-                            <AframeInterview loadedAssets={this.state.interview.loadedAssets}
-                                             updateInterviewCallback={this.updateInterview}
-                                              environment={this.state.interview.loadedEnvironment}
-                                               interviewId={this.id}
-                                               controllerMode={this.state.controllerMode}/>
-                            <Popup trigger={
-                                <Header floated="right" as="h4">
-                                <Icon name="keyboard" />
-                                    CONTROLS
-                                </Header>
-                            }  position="bottom right" content =" Use WASD to move directions while using the webpage. Click the goggles button to enter VR mode. 
-                                                            While in VR, you can interact with assets using the two grab modes described in the configuration box." />
-                                                            <br/>
+                            <VideoComponent />
 
                         </Grid.Row>
                         
@@ -201,3 +190,17 @@ class InterviewPage extends Component {
 }
 
 export default InterviewPage;
+
+{/* <AframeInterview loadedAssets={this.state.interview.loadedAssets}
+                                             updateInterviewCallback={this.updateInterview}
+                                              environment={this.state.interview.loadedEnvironment}
+                                               interviewId={this.id}
+                                               controllerMode={this.state.controllerMode}/>
+                            <Popup trigger={
+                                <Header floated="right" as="h4">
+                                <Icon name="keyboard" />
+                                    CONTROLS
+                                </Header>
+                            }  position="bottom right" content =" Use WASD to move directions while using the webpage. Click the goggles button to enter VR mode. 
+                                                            While in VR, you can interact with assets using the two grab modes described in the configuration box." />
+                                                            <br/> */}
---
+++ src/components/InterviewPage/videoComponent.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import Video from 'twilio-video';
+import axios from 'axios';
+//import RaisedButton from 'material-ui/RaisedButton';
+//import TextField from 'material-ui/TextField';
+//import { Card, CardHeader, CardText } from 'material-ui/Card';
+import { Button, Card, TextArea, Header, } from 'semantic-ui-react';
+import { API_URL } from "../../config/api.config";
+import { safeGetUser } from '../../utils/firebase';
+
+export default class VideoComponent extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            identity: null,
+            roomName: '',
+            roomNameErr: false,
+            previewTracks: null,
+            localMediaAvailable: false,
+            hasJoinedRoom: false,
+            activateRoom: null
+        };
+        this.joinRoom = this.joinRoom.bind(this);
+        this.handleRoomNameChange = this.handleRoomNameChange.bind(this);
+        this.roomJoined = this.roomJoined.bind(this);
+        this.leaveRoom = this.leaveRoom.bind(this);
+        this.detachTracks = this.detachTracks.bind(this);
+        this.detachParticipantTracks = this.detachParticipantTracks.bind(this);
+    }
+
+    componentDidMount() {
+        return safeGetUser().then((user) => user.getIdToken(true)).then((token) => {
+            axios.get(API_URL + '/api/token').then(results => {
+
+                const { identity, token } = results.data;
+                this.setState({ identity, token });
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
+
+    }
+
+    handleRoomNameChange(e) {
+        let roomName = e.target.value;
+        this.setState({ roomName });
+    }
+
+    joinRoom() {
+        if (!this.state.roomName.trim()) {
+            this.setState({ roomaNameErr: true });
+            return;
+        }
+
+        console.log("Joining room '" + this.state.roomName + "'...");
+        let connectOptions = {
+            name: this.state.roomName
+        };
+
+        if (this.state.prviewTracks) {
+            connectOptions.tracks = this.state.previewTracks;
+        }
+
+        Video.connect(this.state.token, connectOptions).then(this.roomJoined, error => {
+            alert('Could not connect to Twilio: ' + error.message);
+        });
+    }
+
+    attachTracks(tracks, container) {
+        tracks.forEach(track => {
+            container.appendChild(track.attach());
+        });
+    }
+
+    attachParticipantsTracks(participant, container) {
+        var tracks = Array.from(participant.tracks.values());
+        this.attachTracks(tracks, container);
+    }
+
+    roomJoined(room) {
+        console.log("Joined as '" + this.state.identity + "'");
+        this.setState({
+            activateRoom: room,
+            localMediaAvailable: true,
+            hasJoinedRoom: true
+        });
+
+        var previewContainer = this.refs.localMedia;
+        if (!previewContainer.querySelector('video')) {
+            this.attachParticipantsTracks(room.localParticipant, previewContainer);
+        }
+
+        room.participants.forEach(participant => {
+            console.log("already in Room '" + participant.identity + "'");
+            var previewContainer = this.refs.remoteMedia;
+            this.attachParticipantsTracks(participant, previewContainer);
+        });
+
+        room.on('participantConnected', participant => {
+            console.log("Joining '" + participant.identity + "'");
+        });
+
+        room.on('trackAdded', (track, participant) => {
+            console.log(participant.identity + ' added track: ' + track.kind);
+            var previewContainer = this.refs.remoteMedia;
+            this.attachTracks([track], previewContainer);
+        });
+
+        room.on('trackRemoved', (track, participant) => {
+            console.log(participant.identity + ' removed track: ' + track.kind);
+            this.detachTracks([track]);
+        });
+
+        room.on('participantDisconnected', participant => {
+            console.log("Participant '" + participant.identity + "' left the room");
+            this.detachParticipantTracks(participant);
+        });
+
+        room.on('disconnected', () => {
+            if (this.state.previewTracks) {
+                this.state.previewTracks.forEach(track => {
+                    track.stop();
+                });
+            }
+            this.detachParticipantTracks(room.localParticipant);
+            room.participants.forEach(this.detachParticipantTracks);
+            this.state.activateRoom = null;
+            this.setState({ hasJoinedRoom: false, localMediaAvailable: false });
+        });
+    }
+
+    leaveRoom() {
+        this.state.activateRoom.disconnect();
+        this.setState({ hasJoinedRoom: false, localMediaAvailable: false });
+    }
+
+    detachTracks(tracks) {
+        tracks.forEach(tracks => {
+            tracks.detach().forEach(detachedElement => {
+                detachedElement.remove();
+            });
+        });
+    }
+
+    detachParticipantTracks(participant) {
+        var tracks = Array.from(participant.tracks.values());
+        this.detachTracks(tracks);
+    }
+
+    render() {
+        let showLocalTrack = this.state.localMediaAvailable ? (
+            <div className="flex-item"><div ref="localMedia" /></div>
+        ) : '';
+        let joinOrLeaveRoomButton = this.state.hasJoinedRoom ? (
+            <Button label="Leave Room" secondary={true} onClick={this.leaveRoom} />) : (
+                <Button label="Join Room" primary={true} onClick={this.joinRoom} />);
+        return (
+            <Card>
+                <Card.Content textAlign="center">
+                    <div className="flex-container">
+                        {showLocalTrack}
+                        <div className="flex-item">
+                            <TextArea placeholder="Room Name" onChange={this.handleRoomNameChange} />
+                            <br />
+                            {joinOrLeaveRoomButton}
+                        </div>
+                        <div className="flex-item" ref="remoteMedia" id="remote-media" />
+                    </div>
+                </Card.Content>
+            </Card>
+        );
+        //errorText={this.state.roomNameErr ? 'Room Name is required' : undefined}
+    }
+}
+
+{/* <Card>
+                <CardText>
+                    <div className="flex-container">
+                        {showLocalTrack}
+                        <div className="flex-item">
+                            <TextField hintText="Room Name" onChange={this.handleRoomNameChange} errorText = {this.state.roomNameErr ? 'Room Name is required' : undefined} />
+                            <br />
+                            {joinOrLeaveRoomButton}
+                        </div>
+                        <div className="flex-item" ref="remoteMedia" id="remote-media" />
+                    </div>
+                </CardText>
+            </Card> */}
