bd2836c158c8504e453b96bdeea1cb4d0c905baf
Austin Wolf <awolfman9@gmail.com>
2019-05-03 15:01:48 -0400

    Comments for firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,14 @@ export const firebaseAuth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 var loading = false;
 
+/**
+ * Handles firebase authentication within the app
+ */
 
+
+ /**
+  * Asynchronously gets the user through a listener, then destroys the listener
+  */
 export function safeGetUser() {
     return new Promise( function(resolve, reject) {
         var unsubscribe = firebaseAuth.onAuthStateChanged((user) => {     
@@ -18,30 +25,14 @@ export function safeGetUser() {
     })
 }
 
+/**
+ * Google Authentication through firebase
+ */
 export function loginWithGoogle() {
     loading = true;
     return firebaseAuth.signInWithRedirect(googleProvider)
-    .then((result) => {
-        /*
-        loading = false;
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        */
-
-    })
     .catch((error) => {
         loading = false;
-        /*
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        */
     });
 }
 
@@ -49,6 +40,9 @@ export function logout() {
     return firebaseAuth.signOut();
 }
 
+/**
+ * Handles Loading while page redirects to home screen after login
+ */
 export function isLoading() {
     return loading;
 }
